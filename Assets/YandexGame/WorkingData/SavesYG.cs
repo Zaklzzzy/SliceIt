e93634910044d2a50b5c8d4a9dbbe444266c9897
Zaklzzzy <zaletin123@gmail.com>
2025-01-27 11:48:11 +0300

    fix: fix save data

--- Assets/YandexGame/WorkingData/SavesYG.cs
+++ Assets/YandexGame/WorkingData/SavesYG.cs
@@ -15,7 +15,7 @@ namespace YG
         //Stats
         public int level = 1;
         public float globalPercentage = 0;
-        public int money = 123500;
+        public int money = 0;
         // UnlockableItems
             public bool[] unlockKnifes = new bool[8];
             public bool[] unlockSliceable = new bool[8];
